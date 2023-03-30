581efd37025363f907753e430468d8c50eced863
Safwannazar <safwannazar99@gmail.com>
2023-03-30 16:02:01 +0800

    Quiz1.js

---
+++ Quiz1.js
@@ -0,0 +1,46 @@
+//let message = 'benr'
+//console.log (message)
+
+//message= 123
+//console.log(message, typeof message)
+
+
+//let str = '1'
+//let num = '1'
+
+//let total = parseInt(str) + parseInt(num)
+//console.log(total)
+
+//let data = ['benr',2343,true]
+
+//console.log(data[0])
+//console.log(data[1])
+//console.log(data[2])
+
+//let user = {
+//    name : 'Safwan',
+ //   age : 24 ,
+  //  faculty : "FKEKK"
+//}
+//console.log(user.name)
+//console.log(user.age)
+//console.log(user.faculty)
+
+let  message= 
+[
+    {
+     name : "USR1",
+      date : "1/4/23",
+      msg : "very good morning",
+      like : 1000 
+
+    },
+    {
+        name : "USR2",
+         date : "1/2/23",
+         msg : "very good morning",
+         like : 12345   
+    },
+]
+
+console.log(message[1].like) // >> very good job
